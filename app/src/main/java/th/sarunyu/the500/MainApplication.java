d692ae3f3b234572365dfe91840bd11aef3fa315
Sarunyu S <sarunyusa@gmail.com>
2016-06-04 11:28:03 +0700

    Initial commit

---
+++ app/src/main/java/th/sarunyu/the500/MainApplication.java
@@ -0,0 +1,9 @@
+package th.sarunyu.the500;
+
+import android.app.Application;
+
+/**
+ * Created by Sarunyu on 4/6/2016.
+ */
+public class MainApplication extends Application {
+}
---
+++ app/src/main/java/th/sarunyu/the500/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package th.sarunyu.the500.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import th.sarunyu.the500.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
